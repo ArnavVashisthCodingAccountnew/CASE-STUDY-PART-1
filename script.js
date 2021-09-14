077c84ba835f521c9a3b7e00c42424f97e426f01
Arnav Vashisth <arnavvst2020@gmail.com>
2021-09-14 16:07:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function disc1(){
+    alert("Microsoft Azure:          Moblinet :      IS MORE ACCURATE");
+}
+function disc2(){
+    alert("Microsoft Azure:          Moblinet :      IS MORE ACCURATE");
+}
+function disc3(){
+    alert("Microsoft Azure:          Moblinet :      IS MORE ACCURATE");
+}
+function disc4(){
+    alert("Microsoft Azure:          Moblinet :      IS MORE ACCURATE");
+}
